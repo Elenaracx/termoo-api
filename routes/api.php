9ea929a285840d36772ad7a96d576d22bebbaef1
Elenaracx <elenara.xavier@alunos.unis.edu.br>
2026-05-24 23:58:18 -0300

    Corrigindo rotas API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JogoController;
 
-Route::post('/iniciar-jogo', [JogoController::class, 'iniciarJogo']);
+Route::post('/start', [JogoController::class, 'iniciarJogo']);
 
-Route::post('/validar-tentativa', [JogoController::class, 'validarTentativa']);
+Route::post('/guess', [JogoController::class, 'validarTentativa']); 
